dba2a2dd61825584bd95b793f99d0ed384c8deb2
Артем <pakshaartem@gmail.com>
2025-05-24 17:39:28 +0300

    fixes

--- src/routers/getDeliveries.ts
+++ src/routers/getDeliveries.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 
 const getDeliveriesRouter = Router();
 
-getDeliveriesRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+getDeliveriesRouter.get("/get-deliveries", (req: Request, res: Response, next: NextFunction) => {
     getUserDeliveries(req, res).catch(next);
 });
 
